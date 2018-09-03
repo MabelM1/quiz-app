968f0144d9c437dd5c565fe577ae8d94748562b8
Mabel <macmota@hotmail.com>
2018-09-03 17:06:06 -0400

    added server.js

--- server.js
+++ server.js
@@ -6,7 +6,37 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const data = require("./src/questions.json");
+
+const fs = require("fs");
+//const os = require('os');
+//const questions = require("src/questions.json");
+
+/*var questionString = fs.readFileSync("./src/questions.json");
+var question = JSON.parse(questionString);
+
+console.log("question " + question[0].question);
+console.log("answer1 " + question[0].answers[1]);
+console.log("answer2 " + question[0].answers[2]);
+console.log("correct " + question[0].correctAnswer);*/
+
+//console.log(typeof question);
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+//app.get("/src/questions.json", (req, res) => {
+app.get("/questions", (req, res) => {
+	//console.log("question " + question[0].question);
+	res.send(data);
+});
 
 app.listen(PORT, () => {
 	console.log(`app started on port ${PORT}`);
