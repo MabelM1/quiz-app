f1e48e63d35b6fc2ba701e4595c1fd53570c9009
Mabel <macmota@hotmail.com>
2018-09-24 22:35:35 -0400

    updated functions to display results

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,7 @@ class Quiz {
 	constructor() {
 		this.intro = document.querySelector(".intro");
 		this.quiz = document.querySelector(".quiz");
+		this.results = document.querySelector(".results");
 		this.startBtn = document.querySelector(".start");
 		this.timer = document.querySelector(".timer");
 		this.question = document.querySelector(".question");
@@ -9,10 +10,16 @@ class Quiz {
 		this.button2 = document.querySelector("#two");
 		this.button3 = document.querySelector("#three");
 		this.button4 = document.querySelector("#four");
+		this.total = document.querySelector(".correct");
+		this.resultmsg = document.querySelector(".result-msg");
+		this.qsNum = document.querySelector(".question-number");
+		this.ansResult = document.querySelector(".answer-result");
 		this.addEventListeners();
 		this.count = 0;
+		this.correct = 0;
 	}
 
+	/* Method that initially loads the data and starts the quiz */
 	startQuiz() {
 		//Hides intro message
 		this.intro.classList.add("disableIntro");
@@ -32,11 +39,11 @@ class Quiz {
 		return this.quizArray;
 	}
 
-	/* Method that fetches data */
+	/* Method that fetches data, stores it in an array and
+	     retrieves the first question     */
 	loadQuestions() {
 		fetch("http://localhost:3000/questions")
 			.then(response => {
-				console.log("inside fetch call");
 				return response.json();
 			})
 			.then(data => {
@@ -49,53 +56,58 @@ class Quiz {
 			});
 	}
 
+	/*  Method displays the questions and the list of answers  */
 	getNewQuestion() {
-		this.startTimer();
+		this.qsNum.innerHTML = `Question ${this.count + 1}/10`;
 		this.question.innerHTML = Quiz.quizArray[this.count].question;
 		this.button1.innerHTML = Quiz.quizArray[this.count].answers[1];
 		this.button2.innerHTML = Quiz.quizArray[this.count].answers[2];
 		this.button3.innerHTML = Quiz.quizArray[this.count].answers[3];
 		this.button4.innerHTML = Quiz.quizArray[this.count].answers[4];
-		console.log("data from questions array ");
 	}
 
+	/*  Method checks if the answer is correct or false, displays it on the screen
+	    and fetches a new question  */
 	checkAnswer(event) {
-		//TODO: when button clicked register if answer is correct or false
-		//if statrt time returns true then increase false and
-		//increase count and get new question
 		let btnClicked = event.target.id;
 
 		if (btnClicked === Quiz.quizArray[this.count].correctAnswer) {
-			console.log(
-				"You are correct" + Quiz.quizArray[this.count].correctAnswer
-			);
+			this.ansResult.innerHTML = "Correct!";
+			this.correct++;
 		} else {
-			console.log("incorrect answer ");
+			this.ansResult.innerHTML = "Incorrect!";
 		}
-		this.count++;
-		this.getNewQuestion();
+		setTimeout(() => {
+			if (this.count === Quiz.quizArray.length - 1) {
+				this.ansResult.innerHTML = "";
+				this.getResults();
+			} else {
+				this.count++;
+				this.ansResult.innerHTML = "";
+				this.getNewQuestion();
+			}
+		}, 1000);
 	}
 
-	//TODO:must be cleared when reach 0 or when user clicks a button
-	startTimer() {
-		let count = 20;
-		//let myVar;
-		//This function will display the count until it is 0
-		const startCount = () => {
-			return count > 0
-				? ((this.timer.innerHTML = count), count--)
-				: //: clearTimeout(myVar);
-				  this.stopTimer(myVar);
-		};
+	/*  Method calculates final result */
+	getResults() {
+		let message = [
+			"You don't know Britney!",
+			"You kind of know Britney!",
+			"You are a true fan!"
+		];
+		this.quiz.classList.add("quiz");
+		this.quiz.classList.remove("quizEnable");
+		this.results.style.display = "block";
+		this.total.innerHTML = `You got ${this.correct} out of 10 right!`;
 
-		let myVar = setInterval(startCount, 1000);
-		console.log("timer started");
-		return true;
-	}
-
-	stopTimer(myVar) {
-		clearTimeout(myVar);
-		console.log("timer stopped");
+		if (this.correct <= 4) {
+			this.resultmsg.innerHTML = ` ${message[0]}`;
+		} else if (this.correct > 4 && this.correct < 7) {
+			this.resultmsg.innerHTML = ` ${message[1]}`;
+		} else {
+			this.resultmsg.innerHTML = ` ${message[2]}`;
+		}
 	}
 
 	/* Method that adds event listeners to various buttons */
